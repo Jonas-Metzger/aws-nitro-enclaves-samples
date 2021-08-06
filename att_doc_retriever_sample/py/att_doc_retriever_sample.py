8bc73969d15d7ac39dd41ab7c27f5184c1d0cb58
Jonas Metzger <40152568+Jonas-Metzger@users.noreply.github.com>
2021-08-06 14:40:01 +0200

    Update att_doc_retriever_sample.py

--- att_doc_retriever_sample/py/att_doc_retriever_sample.py
+++ att_doc_retriever_sample/py/att_doc_retriever_sample.py
@@ -20,7 +20,7 @@ sys.path.insert(0, vsock_dir)
 vs = __import__('vsock-sample')
 
 # Binary executed
-KEY_ARG = 'my public key'
+PUBLIC_KEY = 'my public key'
 USER_DATA = 'more stuff'
 RS_BINARY = './att_doc_retriever_sample/att_doc_retriever_sample'
 
@@ -31,7 +31,7 @@ def client_handler(args):
     client.connect(endpoint)
 
     # Execute binary and send the output to client
-    proc = sp.Popen([RS_BINARY, KEY_ARG, USER_DATA], stdout=sp.PIPE)
+    proc = sp.Popen([RS_BINARY, PUBLIC_KEY, USER_DATA], stdout=sp.PIPE)
     out, err = proc.communicate()
 
     client.send_data(out)
