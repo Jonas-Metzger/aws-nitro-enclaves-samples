92f19643984e12da7f0d7f75a0a514538231a204
Jonas Metzger <40152568+Jonas-Metzger@users.noreply.github.com>
2021-08-06 14:29:03 +0200

    Update main.rs

--- att_doc_retriever_sample/rs/src/main.rs
+++ att_doc_retriever_sample/rs/src/main.rs
@@ -1,15 +1,20 @@
 use nsm_io::Request;
 use serde_bytes::ByteBuf;
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let key_arg = &args[1];
+    let data_arg = &args[2];
+    
     let nsm_fd = nsm_driver::nsm_init();
 
-    let public_key = ByteBuf::from("my super secret key");
-    let hello = ByteBuf::from("hello, world!");
+    let public_key = ByteBuf::from(key_arg);
+    let user_data = ByteBuf::from(data_arg);
 
     let request = Request::Attestation {
         public_key: Some(public_key),
-        user_data: Some(hello),
+        user_data: Some(user_data),
         nonce: None,
     };
 
