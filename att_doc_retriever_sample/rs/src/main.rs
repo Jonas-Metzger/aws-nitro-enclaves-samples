dd386f0f11ebd1d5a17bef5062f1473d15b702d0
Jonas Metzger <40152568+Jonas-Metzger@users.noreply.github.com>
2021-08-08 20:41:14 -0700

    Update main.rs

--- att_doc_retriever_sample/rs/src/main.rs
+++ att_doc_retriever_sample/rs/src/main.rs
@@ -4,13 +4,11 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let key_arg = &args[1];
-    let data_arg = &args[2];
     
     let nsm_fd = nsm_driver::nsm_init();
 
-    let public_key = ByteBuf::from(key_arg);
-    let user_data = ByteBuf::from(data_arg);
+    let public_key = ByteBuf::from(args[1]);
+    let user_data = ByteBuf::from(args[2]);
 
     let request = Request::Attestation {
         public_key: Some(public_key),
